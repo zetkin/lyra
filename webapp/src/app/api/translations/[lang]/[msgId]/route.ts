f3109a250eb1a4beba9b4c6059a5306d1b2f74ea
Richard Olsson <r@richardolsson.se>
2023-10-21 16:54:19 +0200

    Create API resource for saving directly to YAML

---
+++ webapp/src/app/api/translations/[lang]/[msgId]/route.ts
@@ -0,0 +1,42 @@
+import { parse, stringify } from "yaml";
+import fs from "fs/promises";
+import { NextRequest, NextResponse } from "next/server";
+
+const { REPO_PATH } = process.env;
+
+export async function PUT(
+  req: NextRequest,
+  context: { params: { lang: string; msgId: string } },
+) {
+  const payload = await req.json();
+  const { lang, msgId } = context.params;
+  const { text } = payload;
+
+  const yamlPath = REPO_PATH + `/src/locale/${lang}.yml`;
+
+  const yamlBuf = await fs.readFile(yamlPath);
+  const translations = parse(yamlBuf.toString());
+
+  const objKeyPath = msgId.split(".");
+  let curObj = translations;
+  objKeyPath.forEach((key, index) => {
+    if (index == objKeyPath.length - 1) {
+      curObj[key] = text;
+    } else {
+      curObj[key] = { ...curObj[key] };
+      curObj = curObj[key] as Record<string, unknown>;
+    }
+  });
+
+  const yamlOutput = stringify(translations, {
+    singleQuote: true,
+    doubleQuotedAsJSON: true,
+  });
+  await fs.writeFile(yamlPath, yamlOutput);
+
+  return NextResponse.json({
+    lang,
+    msgId,
+    text,
+  });
+}
