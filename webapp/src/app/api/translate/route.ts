1f1408ddf95bdbb3ca15a66c68d7314109fcf8a3
Amer Harb <amer.harb@gmail.com>
2023-10-21 15:38:01 +0200

    flaten yaml

--- webapp/src/app/api/translate/route.ts
+++ webapp/src/app/api/translate/route.ts
@@ -1,6 +1,7 @@
 import fs from "fs/promises";
 import path from "path";
 import { NextResponse } from "next/server";
+import { parse, stringify } from "yaml";
 
 const { REPO_PATH } = process.env;
 
@@ -11,15 +12,17 @@ if (!REPO_PATH) {
 export async function GET() {
   const lang = "en"; //TODO: read from path param
   const yamlFiles: string[] = [];
-  // const messages: MessageData[] = []
+  const translatedArr: Record<string, string>[] = [];
   for await (const item of getMessageFiles(REPO_PATH + "/src", lang)) {
     yamlFiles.push(item);
-    // messages.push(...readTypedMessages(item);
+    const parsed = parse(await fs.readFile(item, "utf-8"));
+    translatedArr.push(flattenObject(parsed));
   }
 
   return NextResponse.json({
     lang,
-    data: yamlFiles,
+    yamlFiles: yamlFiles,
+    translations: Object.assign({}, ...translatedArr),
   });
 }
 
@@ -27,15 +30,42 @@ export async function GET() {
  * Filter only yaml files inside locale folder of xx.yaml or xx.yml
  * @param dirPath
  */
-async function* getMessageFiles(dirPath: string, lang: string): AsyncGenerator<string> {
+async function* getMessageFiles(
+  dirPath: string,
+  lang: string,
+): AsyncGenerator<string> {
   const items = await fs.readdir(dirPath);
   for (const item of items) {
     const itemPath = path.join(dirPath, item);
     const stats = await fs.stat(itemPath);
     if (stats.isDirectory()) {
       yield* getMessageFiles(itemPath, lang);
-    } else if (itemPath.endsWith(`locale/${lang}.yaml`) || itemPath.endsWith(`locale/${lang}.yml`) ) {
+    } else if (
+      itemPath.endsWith(`locale/${lang}.yaml`) ||
+      itemPath.endsWith(`locale/${lang}.yml`)
+    ) {
       yield itemPath;
     }
   }
+}
+
+function flattenObject(
+  obj: Record<string, any>,
+  parentKey: string = "",
+): Record<string, string> {
+  const result: Record<string, any> = {};
+
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      const newKey = parentKey ? `${parentKey}.${key}` : key;
+
+      if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+        Object.assign(result, flattenObject(obj[key], newKey));
+      } else {
+        result[newKey] = obj[key];
+      }
+    }
+  }
+
+  return result;
 }
