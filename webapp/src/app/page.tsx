d03535a13f789dfcc7048d347df4316506d723eb
Richard Olsson <r@richardolsson.se>
2023-10-21 12:24:45 +0200

    Create extremely simple UI to list all messages

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import { MessageData } from "@/utils/readTypedMessages";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [messages, setMessages] = useState<MessageData[]>([]);
+
+  useEffect(() => {
+    async function loadMessages() {
+      const res = await fetch("/api/messages");
+      const payload = await res.json();
+      setMessages(payload.data);
+    }
+
+    loadMessages();
+  }, []);
+
   return (
     <main>
-      <h1>Hello, world</h1>
+      <h1>Messages</h1>
+      <ul>
+        {messages.map((msg) => {
+          return (
+            <li key={msg.id}>
+              <code>{msg.id}</code>
+              <p>{msg.defaultMessage}</p>
+            </li>
+          );
+        })}
+      </ul>
     </main>
   );
 }
