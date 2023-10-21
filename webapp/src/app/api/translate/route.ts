f1613a9211cb038ef6dfe5a0308e42f31d0fa993
Amer Harb <amer.harb@gmail.com>
2023-10-21 13:06:07 +0200

    add translate api

---
+++ webapp/src/app/api/translate/route.ts
@@ -0,0 +1,35 @@
+import fs from "fs/promises";
+import path from "path";
+import { NextResponse } from "next/server";
+
+const { REPO_PATH } = process.env;
+
+if (!REPO_PATH) {
+  throw new Error("REPO_PATH variable not defined");
+}
+
+export async function GET() {
+  const yamlFiles: string[] = [];
+  // const messages: MessageData[] = []
+  for await (const item of getMessageFiles(REPO_PATH + "/src")) {
+    yamlFiles.push(item);
+    // messages.push(...readTypedMessages(item);
+  }
+
+  return NextResponse.json({
+    data: yamlFiles,
+  });
+}
+
+async function* getMessageFiles(dirPath: string): AsyncGenerator<string> {
+  const items = await fs.readdir(dirPath);
+  for (const item of items) {
+    const itemPath = path.join(dirPath, item);
+    const stats = await fs.stat(itemPath);
+    if (stats.isDirectory()) {
+      yield* getMessageFiles(itemPath);
+    } else if (itemPath.endsWith(".yml")) {
+      yield itemPath;
+    }
+  }
+}
