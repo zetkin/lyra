65c99cac83d798e823bbf7edb25937df268b3c95
Amer Harb <amer.harb@gmail.com>
2023-11-12 10:53:58 +0100

    read file and cache in diff module

--- webapp/src/app/api/translations/[lang]/[msgId]/route.ts
+++ webapp/src/app/api/translations/[lang]/[msgId]/route.ts
@@ -1,10 +1,5 @@
-import { envVarNotFound } from "@/utils/util";
-import { parse } from "yaml";
-import fs from "fs/promises";
 import { NextRequest, NextResponse } from "next/server";
-import { LanguageMap } from "@/types";
-
-const REPO_PATH = process.env.REPO_PATH ?? envVarNotFound("REPO_PATH");
+import { getLanguage } from "@/app/api/languages";
 
 export async function PUT(
   req: NextRequest,
@@ -19,35 +14,13 @@ export async function PUT(
   const { lang, msgId } = context.params;
   const { text } = payload;
 
-  let languages: LanguageMap;
-
-  if (!globalThis.languages) {
-    console.debug("Initializing languages");
-    languages = new Map<string, Record<string, unknown>>();
-    globalThis.languages = languages;
-  } else {
-    console.debug("read languages from globalThis");
-    languages = globalThis.languages;
-  }
-  let translations: any;
-  if (!languages.has(lang)) {
-    console.debug("read languages from file");
-    const yamlPath = REPO_PATH + `/src/locale/${lang}.yml`;
-
-    const yamlBuf = await fs.readFile(yamlPath);
-    translations = parse(yamlBuf.toString());
-    languages.set(lang, translations);
-  } else {
-    console.debug("read languages from Memory");
-    translations = languages.get(lang);
-  }
   const objKeyPath = msgId.split(".");
-  let curObj = translations;
+  let curObj = await getLanguage(lang);
   objKeyPath.forEach((key, index) => {
     if (index == objKeyPath.length - 1) {
       curObj[key] = text;
     } else {
-      curObj[key] = { ...curObj[key] };
+      curObj[key] = { ...curObj[key] as Record<string, unknown> };
       curObj = curObj[key] as Record<string, unknown>;
     }
   });
