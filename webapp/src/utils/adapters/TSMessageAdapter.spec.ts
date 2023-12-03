c591b916156cbce85ad60fb4a01f784872765a84
Richard Olsson <r@richardolsson.se>
2023-12-03 14:44:57 +0100

    Refactor TS parsing code to mimic YAML adapter

---
+++ webapp/src/utils/adapters/TSMessageAdapter.spec.ts
@@ -0,0 +1,95 @@
+import { describe, expect, it } from '@jest/globals';
+import mock from 'mock-fs';
+import TSMessageAdapter from './TSMessageAdapter';
+import { MessageData } from '../readTypedMessages';
+
+describe('TSMessageAdapter', () => {
+  describe('getMessages()', () => {
+    it('Finds messageIds.ts file and parses it', async () => {
+      mock({
+        'src/features/something/messageIds.ts': [
+          `import { m, makeMessages } from 'core/i18n';`,
+          `export default makeMessages('feat.something', {`,
+          `  label: m<{ adjective: string }>('My {adjective} feature'),`,
+          `});`,
+        ].join('\n'),
+      });
+
+      const msgAdapter = new TSMessageAdapter();
+      const messages = await msgAdapter.getMessages();
+
+      expect(messages).toEqual(<MessageData[]>[
+        {
+          defaultMessage: 'My {adjective} feature',
+          id: 'feat.something.label',
+          params: [
+            {
+              name: 'adjective',
+              types: ['string'],
+            },
+          ],
+        },
+      ]);
+    });
+
+    it('Finds multiple messageIds files', async () => {
+      mock({
+        'src/features/something/messageIds.ts': [
+          `import { m, makeMessages } from 'core/i18n';`,
+          `export default makeMessages('feat.something', {`,
+          `  label: m<{ adjective: string }>('My {adjective} feature'),`,
+          `});`,
+        ].join('\n'),
+        'src/features/other/messageIds.ts': [
+          `import { m, makeMessages } from 'core/i18n';`,
+          `export default makeMessages('feat.other', {`,
+          `  label: m('My other feature'),`,
+          `});`,
+        ].join('\n'),
+      });
+
+      const msgAdapter = new TSMessageAdapter();
+      const messages = await msgAdapter.getMessages();
+
+      expect(messages).toEqual(<MessageData[]>[
+        {
+          defaultMessage: 'My other feature',
+          id: 'feat.other.label',
+          params: [],
+        },
+        {
+          defaultMessage: 'My {adjective} feature',
+          id: 'feat.something.label',
+          params: [{ name: 'adjective', types: ['string'] }],
+        },
+      ]);
+    });
+
+    it('Finds messageIds files using absolute paths', async () => {
+      mock({
+        '/path/to/src/features/something/messageIds.ts': [
+          `import { m, makeMessages } from 'core/i18n';`,
+          `export default makeMessages('feat.something', {`,
+          `  label: m<{ adjective: string }>('My {adjective} feature'),`,
+          `});`,
+        ].join('\n'),
+      });
+
+      const msgAdapter = new TSMessageAdapter('/path/to/src');
+      const messages = await msgAdapter.getMessages();
+
+      expect(messages).toEqual(<MessageData[]>[
+        {
+          defaultMessage: 'My {adjective} feature',
+          id: 'feat.something.label',
+          params: [
+            {
+              name: 'adjective',
+              types: ['string'],
+            },
+          ],
+        },
+      ]);
+    });
+  });
+});
---
+++ webapp/src/utils/adapters/TSMessageAdapter.ts
@@ -0,0 +1,33 @@
+import readTypedMessages, { MessageData } from '../readTypedMessages';
+import fs from 'fs/promises';
+import path from 'path';
+
+export default class TSMessageAdapter {
+  private basePath: string;
+
+  constructor(basePath: string = 'src') {
+    this.basePath = basePath;
+  }
+
+  async getMessages(): Promise<MessageData[]> {
+    const messages: MessageData[] = [];
+    for await (const item of getMessageFiles(this.basePath)) {
+      messages.push(...readTypedMessages(item));
+    }
+
+    return messages;
+  }
+}
+
+async function* getMessageFiles(dirPath: string): AsyncGenerator<string> {
+  const items = await fs.readdir(dirPath);
+  for (const item of items) {
+    const itemPath = path.join(dirPath, item);
+    const stats = await fs.stat(itemPath);
+    if (stats.isDirectory()) {
+      yield* getMessageFiles(itemPath);
+    } else if (itemPath.endsWith('messageIds.ts')) {
+      yield itemPath;
+    }
+  }
+}
