aee6b9fd1011ef672ea8c96f004fb63828dc3b89
Amer Harb <amer.harb@gmail.com>
2023-12-29 11:48:37 +0100

    remove x.spec.ts

--- webapp/src/utils/x.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from '@jest/globals';
-
-// Trying something here
-describe('dummy describe', () => {
-  it('x', async () => {
-    expect(true).toBeTruthy();
-  });
-});
