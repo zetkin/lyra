03828478db57370701778091e033df1aea185254
Amer Harb <amer.harb@gmail.com>
2023-12-29 14:58:43 +0100

    add skip 2

--- webapp/src/Cache.test.ts
+++ webapp/src/Cache.test.ts
@@ -1,7 +1,7 @@
 import { describe, it } from '@jest/globals';
 
 describe.skip('Cache', () => {
-  describe('gitPull()', () => {
+  describe.skip('gitPull()', () => {
     it.todo('it call gitPull again for different repo path');
     it.todo('it does not call gitPull again for different project in a repo');
     it.todo('it does not call gitPull for same repo path');
