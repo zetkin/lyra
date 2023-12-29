1545fdc541b816becfbe8bb5e390e5c3b1844608
Amer Harb <amer.harb@gmail.com>
2023-12-29 13:57:44 +0100

    add to do test

---
+++ webapp/src/Cache.test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from '@jest/globals';
+
+describe('Cache', () => {
+  describe('gitPull()', () => {
+    it.todo('it call gitPull again for different repo path');
+    it.todo('it does not call gitPull again for different project but same repo');
+    it.todo('it does not call gitPull for same repo path');
+  });
+});
