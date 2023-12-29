5a054bdaeb1fde45a3fdd23b7411099a060714ec
Amer Harb <amer.harb@gmail.com>
2023-12-29 15:03:55 +0100

    delete Cache.test.ts

--- webapp/src/Cache.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it } from '@jest/globals';
-
-describe.skip('Cache', () => {
-  describe.skip('gitPull()', () => {
-    it.todo('it call gitPull again for different repo path');
-    it.todo('it does not call gitPull again for different project in a repo');
-    it.todo('it does not call gitPull for same repo path');
-  });
-});
