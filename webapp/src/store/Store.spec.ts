481217a50ac065b74cf8ff06c2e7bbd8fca219da
Amer Harb <amer.harb@gmail.com>
2024-01-20 15:51:52 +0100

    format

--- webapp/src/store/Store.spec.ts
+++ webapp/src/store/Store.spec.ts
@@ -3,26 +3,28 @@ import { Store } from './Store';
 import { describe, expect, it } from '@jest/globals';
 
 describe('Store', () => {
-  it('get ProjectStore for a path', async () => {
-    const store = new Store();
-    const projectStore = new ProjectStore({
-      getTranslations: async () => ({
-        de: {
-          'greeting.headline': {
-            sourceFile: '',
-            text: 'Hallo',
+  describe('getProjectStore()', () => {
+    it('get ProjectStore for a path', async () => {
+      const store = new Store();
+      const projectStore = new ProjectStore({
+        getTranslations: async () => ({
+          de: {
+            'greeting.headline': {
+              sourceFile: '',
+              text: 'Hallo',
+            },
           },
-        },
-      }),
+        }),
+      });
+      store.addProjectStore('repoPath', 'projectPath', projectStore);
+      const actual = store.getProjectStore('repoPath', 'projectPath');
+      expect(actual).toEqual(projectStore);
     });
-    store.addProjectStore('repoPath', 'projectPath', projectStore);
-    const actual = store.getProjectStore('repoPath', 'projectPath');
-    expect(actual).toEqual(projectStore);
-  });
-  it('throws ProjectStore not found ', async () => {
-    const store = new Store();
-    expect(() => store.getProjectStore('repoPath', 'projectPath')).toThrowError(
-      'ProjectStore not found for path: repoPath/projectPath',
-    );
+    it('throws "ProjectStore not found for ..."', async () => {
+      const store = new Store();
+      expect(() =>
+        store.getProjectStore('repoPath', 'projectPath'),
+      ).toThrowError('ProjectStore not found for path: repoPath/projectPath');
+    });
   });
 });
