90ff29e31c6c70ab7884f44e8bbab08591068e28
Amer Harb <amer.harb@gmail.com>
2024-01-21 17:06:16 +0100

    add comment to TODO:

--- webapp/src/utils/serverConfig.ts
+++ webapp/src/utils/serverConfig.ts
@@ -32,6 +32,7 @@ export class ServerConfig {
 
   public static async read(): Promise<ServerConfig> {
     // TODO: cache this call with TTL, it will be read on every request but only changes when admin changes it
+    //       or only read one time and restart Lyra if change happens, need to make architecture decision about this
     const filename = '../config/projects.yaml';
     try {
       const ymlBuf = await fs.readFile(filename);
