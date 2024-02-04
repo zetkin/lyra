52aaf782debc031de1b74d2b18603a09c25fded4
Amer Harb <amer.harb@gmail.com>
2024-02-04 11:32:12 +0100

    lint

--- webapp/src/utils/unflattenObject.ts
+++ webapp/src/utils/unflattenObject.ts
@@ -3,18 +3,18 @@ interface UnflattenObject {
 }
 
 export function unflattenObject(obj: Record<string, string>): UnflattenObject {
-  const result: UnflattenObject = {}
+  const result: UnflattenObject = {};
   for (const key in obj) {
-    const keys = key.split('.')
-    let current: UnflattenObject = result
+    const keys = key.split('.');
+    let current: UnflattenObject = result;
     for (let i = 0; i < keys.length; i++) {
       if (i === keys.length - 1) {
-        current[keys[i]] = obj[key]
+        current[keys[i]] = obj[key];
       } else {
-        current[keys[i]] = current[keys[i]] || {}
-        current = current[keys[i]] as UnflattenObject
+        current[keys[i]] = current[keys[i]] || {};
+        current = current[keys[i]] as UnflattenObject;
       }
     }
   }
-  return result
+  return result;
 }
