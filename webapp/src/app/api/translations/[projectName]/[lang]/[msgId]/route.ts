f14a1ef6c1cf77f6b7c68542223eee5a1602f58f
Amer Harb <amer.harb@gmail.com>
2024-03-23 01:44:16 +0100

    update TODO

--- webapp/src/app/api/translations/[projectName]/[lang]/[msgId]/route.ts
+++ webapp/src/app/api/translations/[projectName]/[lang]/[msgId]/route.ts
@@ -23,7 +23,7 @@ export async function PUT(
   const { lang, msgId, projectName } = context.params;
   const payload = await req.json();
   const { text } = payload;
-  // TODO: include getProjectConfig & readFromDir in a try/catch block and check for error to return a certain 500 error
+  // TODO: include getProjectConfig() in a try/catch block and check for error to return a certain 500 error
   const serverProjectConfig = await ServerConfig.getProjectConfig(projectName);
   await RepoGit.cloneIfNotExist(serverProjectConfig);
   const repoGit = await RepoGit.getRepoGit(serverProjectConfig);
