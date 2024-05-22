5202b6253cfb95f1c9523dba36d3912f5f9b4aa0
Henry Catalini Smith <henry@catalinismith.com>
2024-05-22 20:35:46 +0200

    Force dynamic rendering on the home page

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -5,6 +5,19 @@ import ProjectsDashboard from '@/components/ProjectsDashboard';
 import { RepoGit } from '@/RepoGit';
 import { ServerConfig } from '@/utils/serverConfig';
 
+// Force dynamic rendering for this page. By default Next.js attempts to render
+// this page statically. That means that it tries to render the page at build
+// time instead of at runtime. That doesn't work: this page needs to fetch
+// project-specific config files and perform git operations. So this little
+// one-liner forces it into dynamic rendering mode.
+// 
+// More info on dynamic vs static rendering at:
+// https://nextjs.org/learn/dashboard-app/static-and-dynamic-rendering
+// 
+// More info on `export const dynamic` at:
+// https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const serverConfig = await ServerConfig.read();
   const projects = await Promise.all(
