bf8fde8caed0dd27ba1133138e7c33e7c2ccfead
WULCAN <7813515+WULCAN@users.noreply.github.com>
2024-06-06 11:10:42 +0200

    Remove fetch for /api/projects

--- webapp/src/app/projects/page.tsx
+++ webapp/src/app/projects/page.tsx
@@ -1,16 +1,4 @@
-'use client';
-
-import { useEffect } from 'react';
-
 export default function Home() {
-  useEffect(() => {
-    async function loadProjects() {
-      await fetch('/api/projects');
-    }
-
-    loadProjects();
-  }, []);
-
   return (
     <main>
       <h1>Projects</h1>
