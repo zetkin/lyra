6fda2dbf776e300b8d8c611b86e3f7f8d4066c21
Henry Catalini Smith <henry@catalinismith.com>
2024-06-18 21:46:24 +0200

    Rewrite home page as a client component

--- webapp/src/types.ts
+++ webapp/src/types.ts
@@ -8,10 +8,42 @@ declare global {
 }
 
 export type ProjectItem = {
+  /**
+   * The URL of the project page.
+   */
+  href: string;
+
+  /**
+   * The project's languages and their translation progress.
+   */
+  languages: {
+    /**
+     * The URL of the page containing the project's messages in this language.
+     */
+
+    href: string;
+
+    /**
+     * The name of the language.
+     */
+    language: string;
+
+    /**
+     * The percentage of messages translated in this language. 0 means none, 100
+     * means all of them.
+     */
+    progress: number;
+  }[];
+
+  /**
+   * The number of messages in the project.
+   */
+  messageCount: number;
+
+  /**
+   * The name of the project.
+   */
   name: string;
-  owner: string;
-  projectPath: string;
-  repo: string;
 };
 
 export type ProjectsResponse = {
