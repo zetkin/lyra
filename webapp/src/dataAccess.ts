3beb4c0e386a344f411365ae5ba0607f08a4514d
WULCAN <7813515+WULCAN@users.noreply.github.com>
2024-07-30 01:02:41 +0200

    Extract readProject

--- webapp/src/dataAccess.ts
+++ webapp/src/dataAccess.ts
@@ -1,27 +1,12 @@
 import { Cache } from '@/Cache';
 import MessageAdapterFactory from '@/utils/adapters/MessageAdapterFactory';
 import { RepoGit } from '@/RepoGit';
-import { ServerConfig } from '@/utils/serverConfig';
+import { ServerConfig, ServerProjectConfig } from '@/utils/serverConfig';
 
 export async function accessProjects() {
   const serverConfig = await ServerConfig.read();
   return serverConfig.projects.map(async (project) => {
-    await RepoGit.cloneIfNotExist(project);
-    const repoGit = await RepoGit.getRepoGit(project);
-    const lyraConfig = await repoGit.getLyraConfig();
-    const projectConfig = lyraConfig.getProjectConfigByPath(
-      project.projectPath,
-    );
-    const msgAdapter = MessageAdapterFactory.createAdapter(projectConfig);
-    const messages = await msgAdapter.getMessages();
-    const store = await Cache.getProjectStore(projectConfig);
-    const languagesWithTranslations = projectConfig.languages.map(
-      async (lang) => {
-        const translations = await store.getTranslations(lang);
-        return { lang, translations };
-      },
-    );
-    return { languagesWithTranslations, messages, name: project.name };
+    return await readProject(project);
   });
 }
 
@@ -35,6 +20,10 @@ export async function accessProject(name: string) {
     return null;
   }
 
+  return readProject(project);
+}
+
+async function readProject(project: ServerProjectConfig) {
   await RepoGit.cloneIfNotExist(project);
   const repoGit = await RepoGit.getRepoGit(project);
   const lyraConfig = await repoGit.getLyraConfig();
