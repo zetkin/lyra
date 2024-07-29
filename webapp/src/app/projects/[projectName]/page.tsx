2d2ae600690357ab015986b589a971a36739d0f4
WULCAN <7813515+WULCAN@users.noreply.github.com>
2024-07-30 00:48:58 +0200

    Extract accessProject

--- webapp/src/app/projects/[projectName]/page.tsx
+++ webapp/src/app/projects/[projectName]/page.tsx
@@ -1,41 +1,20 @@
 import { NextPage } from 'next';
 import { notFound } from 'next/navigation';
 
-import { Cache } from '@/Cache';
+import { accessProject } from '@/dataAccess';
 import { Promises } from '@/utils/Promises';
-import MessageAdapterFactory from '@/utils/adapters/MessageAdapterFactory';
 import ProjectDashboard from '@/components/ProjectDashboard';
-import { RepoGit } from '@/RepoGit';
-import { ServerConfig } from '@/utils/serverConfig';
 
 const ProjectPage: NextPage<{
   params: { projectName: string };
 }> = async ({ params }) => {
   const projectNameParam = params.projectName;
 
-  const serverConfig = await ServerConfig.read();
-  const project = serverConfig.projects.find(
-    (project) => project.name === projectNameParam,
-  );
-
+  const project = await accessProject(projectNameParam);
   if (!project) {
     return notFound();
   }
-
-  await RepoGit.cloneIfNotExist(project);
-  const repoGit = await RepoGit.getRepoGit(project);
-  const lyraConfig = await repoGit.getLyraConfig();
-  const projectConfig = lyraConfig.getProjectConfigByPath(project.projectPath);
-  const msgAdapter = MessageAdapterFactory.createAdapter(projectConfig);
-  const messages = await msgAdapter.getMessages();
-  const store = await Cache.getProjectStore(projectConfig);
-  const languagesWithTranslations = projectConfig.languages.map(
-    async (lang) => {
-      const translations = await store.getTranslations(lang);
-      return { lang, translations };
-    },
-  );
-  const projectName = project.name;
+  const { projectName, messages, languagesWithTranslations } = project;
   const languages = await new Promises(languagesWithTranslations)
     .map(({ lang, translations }) => {
       return {
--- webapp/src/dataAccess.ts
+++ webapp/src/dataAccess.ts
@@ -24,3 +24,30 @@ export async function accessProjects() {
     return { languagesWithTranslations, messages, name: project.name };
   });
 }
+
+export async function accessProject(projectNameParam: string) {
+  const serverConfig = await ServerConfig.read();
+  const project = serverConfig.projects.find(
+    (project) => project.name === projectNameParam,
+  );
+
+  if (!project) {
+    return null;
+  }
+
+  await RepoGit.cloneIfNotExist(project);
+  const repoGit = await RepoGit.getRepoGit(project);
+  const lyraConfig = await repoGit.getLyraConfig();
+  const projectConfig = lyraConfig.getProjectConfigByPath(project.projectPath);
+  const msgAdapter = MessageAdapterFactory.createAdapter(projectConfig);
+  const messages = await msgAdapter.getMessages();
+  const store = await Cache.getProjectStore(projectConfig);
+  const languagesWithTranslations = projectConfig.languages.map(
+    async (lang) => {
+      const translations = await store.getTranslations(lang);
+      return { lang, translations };
+    },
+  );
+  const projectName = project.name;
+  return { languagesWithTranslations, messages, projectName };
+}
