034bc7e95e4a5aebfef4288a601ac131b9ddbd7d
WULCAN <7813515+WULCAN@users.noreply.github.com>
2024-08-18 17:08:27 +0200

    Rename field
    It is more important to highlight that the field has a different type
    than that the field too contains promises.

--- webapp/src/utils/Promises.ts
+++ webapp/src/utils/Promises.ts
@@ -1,15 +1,15 @@
 export class Promises<T> {
-  private constructor(private promises: Array<Promise<T>>) {}
+  private constructor(private array: Array<Promise<T>>) {}
 
   static of<T>(array: Array<Promise<T>>) {
     return new Promises(array);
   }
 
   map<U>(callbackFn: (value: T) => U | Promise<U>): Promises<U> {
-    return new Promises(this.promises.map((p) => p.then(callbackFn)));
+    return new Promises(this.array.map((p) => p.then(callbackFn)));
   }
 
   all() {
-    return Promise.all(this.promises);
+    return Promise.all(this.array);
   }
 }
