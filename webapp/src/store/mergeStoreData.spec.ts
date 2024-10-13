02450ed8cddd35ebd65a29a9f5be7890b064e13a
Richard Olsson <r@richardolsson.se>
2024-10-13 14:53:39 +0200

    Implement cache merge algorithm as mergeStoreData()

---
+++ webapp/src/store/mergeStoreData.spec.ts
@@ -0,0 +1,125 @@
+import { describe, expect, it } from '@jest/globals';
+
+import mergeStoreData from './mergeStoreData';
+import { StoreData } from './types';
+import { MessageData, MessageTranslation } from '@/utils/adapters';
+
+describe('mergeStoreData()', () => {
+  it('returns empty when both arguments are empty', () => {
+    const inMemory: StoreData = {
+      languages: {},
+      messages: [],
+    };
+
+    const fromRepo: StoreData = {
+      languages: {},
+      messages: [],
+    };
+
+    const result = mergeStoreData(inMemory, fromRepo);
+
+    expect(result).toEqual(fromRepo);
+  });
+
+  it('accepts fromRepo when inMemory is empty', () => {
+    const inMemory: StoreData = {
+      languages: {},
+      messages: [],
+    };
+
+    const fromRepo: StoreData = {
+      languages: {
+        sv: {
+          'project.title': mockTranslation('Zetkin'),
+        },
+      },
+      messages: [mockMessage('project.title', 'Zetkin')],
+    };
+
+    const result = mergeStoreData(inMemory, fromRepo);
+    expect(result).toEqual(fromRepo);
+  });
+
+  it('accepts new message', () => {
+    const inMemory: StoreData = {
+      languages: {
+        sv: {
+          'project.title': mockTranslation('Zetkin'),
+        },
+      },
+      messages: [mockMessage('project.title', 'Zetkin')],
+    };
+
+    const fromRepo: StoreData = {
+      languages: {
+        sv: {
+          'project.author': mockTranslation('Zetkin Foundation'),
+          'project.title': mockTranslation('Zetkin'),
+        },
+      },
+      messages: [
+        mockMessage('project.title', 'Zetkin'),
+        mockMessage('project.author', 'Zetkin Foundation'),
+      ],
+    };
+
+    const result = mergeStoreData(inMemory, fromRepo);
+    expect(result).toEqual(fromRepo);
+  });
+
+  it('removes translations when message disappears from repo', () => {
+    const inMemory: StoreData = {
+      languages: {
+        sv: {
+          'project.title': mockTranslation('Zetkin'),
+        },
+      },
+      messages: [mockMessage('project.title', 'Zetkin')],
+    };
+
+    const fromRepo: StoreData = {
+      languages: {},
+      messages: [],
+    };
+
+    const result = mergeStoreData(inMemory, fromRepo);
+    expect(result).toEqual(fromRepo);
+  });
+
+  it('retains translation from memory', () => {
+    const inMemory: StoreData = {
+      languages: {
+        sv: {
+          'project.title': mockTranslation('Zetkin'),
+        },
+      },
+      messages: [mockMessage('project.title', 'Zetkin')],
+    };
+
+    const fromRepo: StoreData = {
+      languages: {
+        sv: {
+          'project.title': mockTranslation('The Zetkin'),
+        },
+      },
+      messages: [mockMessage('project.title', 'Zetkin')],
+    };
+
+    const result = mergeStoreData(inMemory, fromRepo);
+    expect(result).toEqual(inMemory);
+  });
+});
+
+const mockTranslation = (
+  text: MessageTranslation['text'],
+  sourceFile: MessageTranslation['sourceFile'] = 'thefile.yml',
+) => ({
+  sourceFile,
+  text,
+});
+
+const mockMessage = (
+  defaultMessage: MessageData['defaultMessage'],
+  id: MessageData['id'],
+  params: MessageData['params'] = [],
+) => ({ defaultMessage, id, params });
