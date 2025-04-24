2757f5270e5b42df275b99c3b17e3ec148cccf1e
Alexander Schreiner <github@alexander-schreiner.de>
2025-04-24 22:49:17 +0200

    add comment for webappAbsPath
    Signed-off-by: Alexander Schreiner <github@alexander-schreiner.de>

--- webapp/src/utils/paths.ts
+++ webapp/src/utils/paths.ts
@@ -3,6 +3,8 @@ import path from 'path';
 const projectsYamlRelPath = 'config/projects.yaml';
 const projectsYmlRelPath = 'config/projects.yml';
 const lyraProjectsFolder = 'lyra-projects';
+// Despite our Dockerfile using WORKDIR /app, cwd will be /app/webapp in our container because our next build
+// generates a server.js with process.chdir(__dirname).
 const webappAbsPath = process.cwd();
 const lyraAbsPath = path.resolve(webappAbsPath, '..');
 const projectsYamlAbsPath = path.resolve(lyraAbsPath, projectsYamlRelPath);
