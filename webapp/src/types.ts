28d911a40a6f0fa57107c1b065602705bc4565d2
Amer Harb <me@amerharb.com>
2025-08-31 14:35:34 +0200

    remove unused type

--- webapp/src/types.ts
+++ webapp/src/types.ts
@@ -1,7 +1,5 @@
 import { Store } from '@/store/Store';
 
-export type LanguageMap = Map<string, Record<string, string>>;
-
 declare global {
   /**
    * Either we promised to complete initialization,
