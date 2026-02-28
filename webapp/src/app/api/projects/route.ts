8d4a7801c993dc1e60ecfb3a2306db7b365c4ac6
Henry Catalini Smith <henry@catalinismith.se>
2026-02-28 10:25:57 +0100

    Split the front page into a separate backend and frontend

---
+++ webapp/src/app/api/projects/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { Promises } from '@/utils/Promises';
+import { accessProjects } from '@/dataAccess';
+import { ProjectCardProps } from '@/components/ProjectCard';
+
+type ProjectsResponse = ProjectCardProps[];
+
+export async function GET(
+  req: NextRequest,
+): Promise<NextResponse<ProjectsResponse>> {
+  const projectData = await accessProjects();
+
+  const projects = await Promises.of(projectData)
+    .map(async ({ name, messages, languagesWithTranslations }) => {
+      const languages = await Promises.of(languagesWithTranslations)
+        .map(({ lang, translations }) => ({
+          href: `/projects/${name}/${lang}`,
+          language: lang,
+          progress: translations
+            ? (Object.keys(translations).length / messages.length) * 100
+            : 0,
+        }))
+        .all();
+
+      return {
+        href: `/projects/${name}`,
+        languages,
+        messageCount: messages.length,
+        name,
+      };
+    })
+
+    .all();
+
+  return NextResponse.json(projects);
+}
