c927c9a661479eb7413b4527d62f68b1ac4ae73a
Amer Harb <me@amerharb.com>
2026-05-17 14:29:16 +0200

    do static for methods

--- webapp/src/dao/TranslationDAO.ts
+++ webapp/src/dao/TranslationDAO.ts
@@ -1,39 +1,39 @@
-import { BaseDAO, getDb } from './db';
+import { BaseDAO } from './db';
 import { TranslateState, Translation } from './types';
 
 export class TranslationDAO extends BaseDAO {
-  find(keyId: number, langId: string): Translation | undefined {
-    return getDb()
+  static find(keyId: number, langId: string): Translation | undefined {
+    return this.db
       .prepare(
         'SELECT id, key, lang, text, state FROM translation WHERE key = ? AND lang = ?',
       )
       .get(keyId, langId) as Translation | undefined;
   }
 
-  upsert(
+  static upsert(
     keyId: number,
     langId: string,
     text: string,
     state: TranslateState,
   ): Translation {
-    getDb()
+    this.db
       .prepare(
         `INSERT INTO translation (key, lang, text, state) VALUES ($key, $lang, $text, $state)
          ON CONFLICT(key, lang) DO UPDATE SET text = excluded.text, state = excluded.state`,
       )
       .run({ key: keyId, lang: langId, state: state, text: text });
-    return getDb()
+    return this.db
       .prepare(
         'SELECT id, key, lang, text, state FROM translation WHERE key = ? AND lang = ?',
       )
       .get(keyId, langId) as Translation;
   }
 
-  findByProject(
+  static findByProject(
     projectId: number,
     langId: string,
   ): Array<{ key: string; path: string; state: TranslateState; text: string }> {
-    return getDb()
+    return this.db
       .prepare(
         `SELECT p.value AS path, k.value AS key, t.text, t.state
          FROM translation t
@@ -49,11 +49,11 @@ export class TranslationDAO extends BaseDAO {
     }>;
   }
 
-  countTranslationForProjectForLanguage(
+  static countTranslationForProjectForLanguage(
     projectId: number,
     langId: string,
   ): number {
-    const result = getDb()
+    const result = this.db
       .prepare(
         `SELECT COUNT(1) AS c
          FROM translation t
