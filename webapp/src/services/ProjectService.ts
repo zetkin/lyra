97f78294f88d910b4b2a247f7a3ddcac96410892
Amer Harb <me@amerharb.com>
2026-05-17 14:29:54 +0200

    do getProjects WiP

--- webapp/src/services/ProjectService.ts
+++ webapp/src/services/ProjectService.ts
@@ -1,17 +1,30 @@
-import { ProjectDto } from '@/dto/ProjectDto';
+import { LanguageWithProgress, ProjectDto } from '@/dto/ProjectDto';
 import { ProjectsDAO } from '@/dao/ProjectsDAO';
+import { TranslationDAO } from '@/dao/TranslationDAO';
+import { ProjectLangDAO } from '@/dao/ProjectLangDAO';
 
 export class ProjectService {
   public getProjects(): ProjectDto[] {
-    /*
-           progress: translations
-            ? (Object.keys(translations).length / messages.length) * 100
-            : 0,
-    * */
     const projects = ProjectsDAO.findAll();
-    const projectDtos: ProjectDto[];
+    const projectDtos: ProjectDto[] = [];
     for (const project of projects) {
-      int count
+      const langs = ProjectLangDAO.findLangsByProject(project.id); // TODO: collect the languages of a project
+      const languages: LanguageWithProgress[] = [];
+      for (const lang of langs) {
+        const transCount = TranslationDAO.countTranslationForProjectForLanguage(
+          project.id,
+          lang.id,
+        );
+        languages.push({
+          language: lang.name,
+          progress: transCount / messageCount, //TODO: messageCount
+        });
+      }
+
+      const name = project.name;
+      projectDtos.push({ languages, messageCount, name });
     }
+
+    return projectDtos;
   }
 }
