1ea7cfb000dbe5b2be9d17c29302db2e2264f77a
Amer Harb <me@amerharb.com>
2026-05-17 13:59:45 +0200

    countTranslationForProjectForLanguage

--- webapp/src/dao/TranslationDAO.ts
+++ webapp/src/dao/TranslationDAO.ts
@@ -1,9 +1,9 @@
-import { BaseDAO } from './db';
+import { BaseDAO, getDb } from './db';
 import { TranslateState, Translation } from './types';
 
 export class TranslationDAO extends BaseDAO {
   find(keyId: number, langId: string): Translation | undefined {
-    return this.db
+    return getDb()
       .prepare(
         'SELECT id, key, lang, text, state FROM translation WHERE key = ? AND lang = ?',
       )
@@ -16,13 +16,13 @@ export class TranslationDAO extends BaseDAO {
     text: string,
     state: TranslateState,
   ): Translation {
-    this.db
+    getDb()
       .prepare(
         `INSERT INTO translation (key, lang, text, state) VALUES ($key, $lang, $text, $state)
          ON CONFLICT(key, lang) DO UPDATE SET text = excluded.text, state = excluded.state`,
       )
       .run({ key: keyId, lang: langId, state: state, text: text });
-    return this.db
+    return getDb()
       .prepare(
         'SELECT id, key, lang, text, state FROM translation WHERE key = ? AND lang = ?',
       )
@@ -33,7 +33,7 @@ export class TranslationDAO extends BaseDAO {
     projectId: number,
     langId: string,
   ): Array<{ key: string; path: string; state: TranslateState; text: string }> {
-    return this.db
+    return getDb()
       .prepare(
         `SELECT p.value AS path, k.value AS key, t.text, t.state
          FROM translation t
@@ -47,5 +47,23 @@ export class TranslationDAO extends BaseDAO {
       state: TranslateState;
       text: string;
     }>;
+  }
+
+  countTranslationForProjectForLanguage(
+    projectId: number,
+    langId: string,
+  ): number {
+    const result = getDb()
+      .prepare(
+        `SELECT COUNT(1) AS c
+         FROM translation t
+         LEFT JOIN i18n_key k ON k.id = t.key
+         LEFT JOIN path p ON p.id = k.path
+      
+         WHERE p.project = ? AND t.lang = ?`,
+      )
+      .all(projectId, langId) as Array<{ c: number }>;
+
+    return (result?.length ?? 0) > 0 ? result[0].c : 0;
   }
 }
