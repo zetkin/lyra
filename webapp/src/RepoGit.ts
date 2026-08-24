da4638242c7e7182b08316d4969a670add3a1027
Vector-Hector <30576258+Vector-Hector@users.noreply.github.com>
2026-08-24 10:32:14 +0200

    fix embedded state in translation files

--- webapp/src/RepoGit.ts
+++ webapp/src/RepoGit.ts
@@ -8,7 +8,7 @@ import { LyraConfig } from '@/utils/lyraConfig';
 import packageJson from '../package.json';
 import { ServerProjectConfig } from '@/utils/serverConfig';
 import { SimpleGitWrapper } from '@/utils/git/SimpleGitWrapper';
-import { unflattenTranslateIdTextState } from '@/utils/unflattenObject';
+import { unflattenObject } from '@/utils/unflattenObject';
 import { debug, error, info, warn } from '@/utils/log';
 import { WriteLanguageFileError, WriteLanguageFileErrors } from '@/errors';
 import { type TranslationMap } from '@/utils/adapters';
@@ -216,13 +216,14 @@ export class RepoGit {
           Object.entries(translationsBySourceFile).map(
             async ([sourceFile, translation]) => {
               const yamlPath = path.join(translationsPath, sourceFile);
-              const yamlOutput = stringify(
-                unflattenTranslateIdTextState(translation),
-                {
-                  doubleQuotedAsJSON: true,
-                  singleQuote: true,
-                },
-              );
+              const textById: Record<string, string> = {};
+              Object.entries(translation).forEach(([id, textState]) => {
+                textById[id] = textState.text;
+              });
+              const yamlOutput = stringify(unflattenObject(textById), {
+                doubleQuotedAsJSON: true,
+                singleQuote: true,
+              });
               try {
                 await fs.writeFile(yamlPath, yamlOutput, { flush: true });
                 info(`Successfully wrote to: ${yamlPath}`);
